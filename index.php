ab2c20c69bd62e49bcd058b7c0db9e22783a61cc
pablox946 <pablomendes1997@hotmail.com>
2021-05-11 17:25:06 -0300

    Adição do codigin frifas

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disfunção Eretíl</title>
+    <link rel="stylesheet" type="text/css" href="./spile.css" media="screen"> 
+</head>
+
+<body class="bd">
+    <div class="tela">
+        <h1>Cadastro de rola</h1>
+        <label>Cadastre sua Disfunção Eretil aqui:</label>
+        <select>
+            <option>pinto torto</option>
+            <option>pinto arredondado</option>
+            <option>pinto fraturento</option>
+            <option>pinto doentil</option>
+            <option>pinto alcoolatra</option>
+            <option>pinto usuario de drogas</option>
+        </select> <br>
+        <label>Cadrasto seu sentimento</label> <br>
+        <input type="checkbox">Boomer de zap
+        <input type="checkbox">Jogador de Free Fire <br>
+        <input type="checkbox">Bolsominon
+        <input type="checkbox">Fumante de Derby <br>
+        <input type="checkbox">Mono Yasuo Player X1 lixo <br>
+
+    </div>
+
+    <div>
+        <button class="botao">CONFIRMAR</button>
+
+    </div>
+
+
+</body>
+
+</html>
